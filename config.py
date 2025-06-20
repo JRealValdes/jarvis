1e49ec6866afe6ae4f76966adf387e2d0fc06842
Javier Real <jrealvaldes@gmail.com>
2025-06-20 20:12:55 +0200

    Hostile responses mode

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 from enums.core_enums import ModelEnum, IdentificationFailedProtocolEnum
 
 DEFAULT_MODEL = ModelEnum.GPT_3_5
-IDENTIFICATION_FAILED_PROTOCOL = IdentificationFailedProtocolEnum.AUTOMATIC_RESPONSE
+IDENTIFICATION_FAILED_PROTOCOL = IdentificationFailedProtocolEnum.HOSTILE_RESPONSES
 DB_DEBUG_MODE = True
