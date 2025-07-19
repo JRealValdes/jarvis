f98797c7fc0677deefe76dde14f9050b56cd52b0
Javier Real <jrealvaldes@gmail.com>
2025-07-19 18:29:06 +0200

    Avoid reloading api exec

--- api/main_api.py
+++ api/main_api.py
@@ -64,4 +64,4 @@ async def reset_memory():
     return {"status": "ok", "message": "Memory reset"}
 
 if __name__ == "__main__":
-    uvicorn.run("api.main_api:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("api.main_api:app", host="0.0.0.0", port=8000)
